911a665a79749944e927bb913bfd570b36731516
Kristi Kacaj <kristik.web@gmail.com>
2020-07-29 21:29:03 -0400

    home.html complete

---
+++ app/data/friends.js
@@ -0,0 +1,101 @@
+var friends = [
+    {
+      name: "Lebron James",
+      photo: "https://cloudfront-us-east-1.images.arcpublishing.com/tbt/AGKGYQKMZRG5PG7OJH47XOPGXM.jpg",
+      scores: [
+        "3",
+        "1",
+        "4",
+        "2",
+        "2",
+        "1",
+        "3",
+        "5",
+        "4",
+        "4"
+      ]
+    },
+    {
+      name: "Michael Jackson",
+      photo: "https://i.pinimg.com/originals/c3/52/80/c352801bdf8686fd2d4c06e984c16d79.jpg",
+      scores: [
+        "2",
+        "2",
+        "1",
+        "1",
+        "3",
+        "5",
+        "4",
+        "2",
+        "4",
+        "1"
+      ]
+    },
+    {
+      name: "Hugh Jackman",
+      photo: "https://m.media-amazon.com/images/M/MV5BNDExMzIzNjk3Nl5BMl5BanBnXkFtZTcwOTE4NDU5OA@@._V1_.jpg",
+      scores: [
+        "5",
+        "4",
+        "3",
+        "4",
+        "3",
+        "5",
+        "4",
+        "1",
+        "2",
+        "5"
+      ]
+    },
+    {
+      name: "Ryan Reynolds",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg/1200px-Deadpool_2_Japan_Premiere_Red_Carpet_Ryan_Reynolds_%28cropped%29.jpg",
+      scores: [
+        "3",
+        "1",
+        "4",
+        "3",
+        "2",
+        "4",
+        "3",
+        "5",
+        "2",
+        "3"
+      ]
+    },
+    {
+      name: "Beyonce",
+      photo: "https://www.etonline.com/sites/default/files/styles/video_1920x1080/public/images/2020-06/eto_trending_bet_awards_beyonce_062820_vidpic.jpg?h=d1cb525d&itok=GIBn7K5j",
+      scores: [
+        "5",
+        "2",
+        "4",
+        "2",
+        "4",
+        "2",
+        "4",
+        "1",
+        "1",
+        "3"
+      ]
+    },
+    {
+      name: "Johnny Depp",
+      photo: "https://m.media-amazon.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_.jpg",
+      scores: [
+        "3",
+        "5",
+        "2",
+        "2",
+        "3",
+        "2",
+        "2",
+        "4",
+        "4",
+        "5"
+      ]
+    }
+  
+  ];
+
+  module.exports = friends;
